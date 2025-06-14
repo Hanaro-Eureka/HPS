7cd5d271969e4c807233e709b1b7e0464bac3d01
kimkiyeonn <paynjoy2@naver.com>
2025-06-14 18:12:39 +0900

    Chore: 페이지 테스트 코드 제거

--- hps/app/page.tsx
+++ hps/app/page.tsx
@@ -1,11 +1,8 @@
 'use client';
 
-import Checkbox from '@/components/atoms/Checkbox';
 import Image from 'next/image';
-import { ChangeEvent, useState } from 'react';
 
 export default function Home() {
-  const [isChecked, setIsChecked] = useState(false);
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
