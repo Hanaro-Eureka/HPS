5a7e4ff4c55b90bd4b9b395ee03fc16f8cd2f8a7
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-06-16 00:07:40 +0900

    feat: category icon 생성

---
+++ hps/constants/categoryIcons.ts
@@ -0,0 +1,23 @@
+import hair from './ic_beautysalon.svg';
+import cafe from './ic_coffee.svg';
+import company from './ic_company.svg';
+import store from './ic_convenience_store.svg';
+import etc from './ic_etc.svg';
+import pub from './ic_pub.svg';
+import restaurant from './ic_restaurant.svg';
+import shopping from './ic_shopping.svg';
+import transportation from './ic_transportation.svg';
+
+const icons = {
+  hair,
+  cafe,
+  company,
+  store,
+  etc,
+  pub,
+  restaurant,
+  shopping,
+  transportation,
+};
+
+export default icons;
