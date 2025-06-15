92eac8307dc47d169d436ec5b0c4693a473cf936
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-06-16 00:11:58 +0900

    rename: svg 파일 경로 수정

--- hps/constants/categoryIcons.ts
+++ hps/constants/categoryIcons.ts
@@ -1,12 +1,12 @@
-import hair from './ic_beautysalon.svg';
-import cafe from './ic_coffee.svg';
-import company from './ic_company.svg';
-import store from './ic_convenience_store.svg';
-import etc from './ic_etc.svg';
-import pub from './ic_pub.svg';
-import restaurant from './ic_restaurant.svg';
-import shopping from './ic_shopping.svg';
-import transportation from './ic_transportation.svg';
+import hair from '@/assets/svgs/ic_beautysalon.svg';
+import cafe from '@/assets/svgs/ic_coffee.svg';
+import company from '@/assets/svgs/ic_company.svg';
+import store from '@/assets/svgs/ic_convenience_store.svg';
+import etc from '@/assets/svgs/ic_etc.svg';
+import pub from '@/assets/svgs/ic_pub.svg';
+import restaurant from '@/assets/svgs/ic_restaurant.svg';
+import shopping from '@/assets/svgs/ic_shopping.svg';
+import transportation from '@/assets/svgs/ic_transportation.svg';
 
 const icons = {
   hair,
