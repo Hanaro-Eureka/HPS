f3ef3529b5acada9376eb068c1969ef51f09cc79
bokyeommmmm <bokyeommmmm@github.com>
2025-06-17 11:03:51 +0900

    Design: 그래프 테두리 수정

--- hps/components/molcules/SalaryBarGraph.tsx
+++ hps/components/molcules/SalaryBarGraph.tsx
@@ -11,33 +11,31 @@ import {
 } from 'recharts';
 
 type Props = {
-  //사용한 금액 , 남은 금액, x축 label
   data: { used: number; remain: number; name?: string }[];
   width?: number;
   height?: number;
   className?: string;
-  colors: { used: string; remain: string };
+  colors: { used: string; remain: string; lStroke: string; rStroke: string };
 };
 
 const CustomBar = (
-  //직접 SVG 경로를 그리는 커스텀 막대
-  props: Partial<BarProps> & { radius: [number, number, number, number] }
+  props: Partial<BarProps> & {
+    radius: [number, number, number, number];
+    stroke?: string;
+  }
 ) => {
   const {
-    //막대의 위치, 크기, 채울 색, 모서리 둥근 값.
     x = 0,
     y = 0,
     width = 0,
     height = 0,
     fill = '#000',
+    stroke,
     radius = [0, 0, 0, 0],
   } = props;
 
-  //top left, top right ...
   const [tl, tr, br, bl] = radius;
 
-  //SVG path 문자열로 둥근 사각형을 직접 그림
-  //각 꼭짓점 radius 있으면 둥글게 아님 직각.
   const path = `
     M${Number(x) + tl},${y}
     h${width - tl - tr}
@@ -55,9 +53,8 @@ const CustomBar = (
     <path
       d={path}
       fill={fill}
-      //테두리 색
-      stroke={fill === '#FFFFFF' ? '#56B8AB' : fill}
-      strokeWidth={1}
+      stroke={stroke ?? 'none'}
+      strokeWidth={stroke ? 2 : 0}
     />
   );
 };
@@ -75,24 +72,34 @@ export default function SalaryBarChart({
         layout='vertical'
         width={width}
         data={data}
-        margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
+        margin={{ top: 0, right: 2, left: 2, bottom: 0 }}
         className={className}
       >
         <XAxis type='number' hide />
         <YAxis type='category' dataKey='name' hide />
         <Tooltip formatter={(value: number) => value.toLocaleString() + '원'} />
         <Bar
-          //막대그래프 왼쪽 파트
           dataKey='used'
           stackId='a'
-          shape={<CustomBar radius={[10, 0, 0, 10]} />}
+          shape={
+            <CustomBar
+              radius={[10, 0, 0, 10]}
+              fill={colors.used}
+              stroke={colors.lStroke} // 왼쪽 테두리 색상
+            />
+          }
           fill={colors.used}
         />
         <Bar
-          //오른쪽 파트
           dataKey='remain'
           stackId='a'
-          shape={<CustomBar radius={[0, 10, 10, 0]} />}
+          shape={
+            <CustomBar
+              radius={[0, 10, 10, 0]}
+              fill={colors.remain}
+              stroke={colors.rStroke}
+            />
+          }
           fill={colors.remain}
         />
       </BarChart>
