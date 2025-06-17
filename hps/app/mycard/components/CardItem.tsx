1c5a11c5d5f6ae5d8f2748e9186b32e846b01b7b
kimkiyeonn <paynjoy2@naver.com>
2025-06-17 20:22:54 +0900

    Design: 카드 컴포넌트 위치 수정

--- hps/app/mycard/components/CardItem.tsx
+++ hps/app/mycard/components/CardItem.tsx
@@ -1,7 +1,5 @@
-'use client';
-
 import Text from '@/components/atoms/Text';
-import SalaryBarGraph from '@/components/molcules/SalaryBarGraph';
+import SalaryBarChart from '@/components/molcules/SalaryBarGraph';
 import { getTotalSpent } from '../utils/cardResult';
 
 type Props = {
@@ -21,21 +19,27 @@ export default function CardItem({ idx, cardName, cardAmount }: Props) {
 
   return (
     <div className='flex py-7 pl-7 pr-3 justify-between bg-white rounded-lg [box-shadow:var(--shadow-taxbox)]'>
-      <img
-        src={cardImages[idx]}
-        alt={`카드${idx + 1} 이미지`}
-        className='items-center justify-center'
-      />
+      <div className='flex pl-3 items-center justify-center'>
+        <img src={cardImages[idx]} alt={`카드${idx + 1} 이미지`} />
+      </div>
 
-      <div className='flex flex-col w-46 justify-start'>
-        <Text className='text-base font-[400]'>{cardName}</Text>
-        <SalaryBarGraph
-          data={[{ name: '전체 카드', used: 500000, remain: 200000 }]}
+      <div className='flex flex-col w-full justify-start'>
+        <Text className='pt-3 pl-5 text-base text-black-font font-[400]'>
+          {cardName}
+        </Text>
+        <SalaryBarChart
+          data={[
+            {
+              moreSpent: usedAmount,
+              leastSpent: Math.max(cardAmount - usedAmount, 0),
+            },
+          ]}
           colors={{
-            used: 'var(--hana-green)',
-            remain: '#FFFFFF',
+            moreSpent: 'var(--hana-green)',
+            leastSpent: '#FFFFFF',
           }}
-          height={100}
+          width={300} //안먹어요
+          height={60}
         />
         {isAchieved ? (
           <Text className='text-xs font-[400] text-hana-green text-center'>
--- hps/app/mycard/page.tsx
+++ hps/app/mycard/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import Title from '@/components/atoms/Title';
 import { cardData } from '@/constants/cardData';
 import CardItem from './components/CardItem';
@@ -7,7 +5,10 @@ import CardItem from './components/CardItem';
 export default function CardPage() {
   return (
     <main className='items-center justify-start max-w-md mx-auto p-6 space-y-6'>
-      <Title tag='h1' className='text-2xl font-[600] text-center pb-14'>
+      <Title
+        tag='h1'
+        className='text-2xl text-black-font font-[600] text-center pb-14'
+      >
         나의 카드 실적 현황
       </Title>
       {cardData.map((card, idx) => (
