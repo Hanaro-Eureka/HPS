0e4ec8c1e538fde67bcc4d942c2ede3c643cb2c3
dlwlals1289 <dlwlals1289@gmail.com>
2025-06-17 10:40:46 +0900

    HOTFIX: Barchart width 적용 문제

--- hps/components/molcules/BarGraph.tsx
+++ hps/components/molcules/BarGraph.tsx
@@ -18,7 +18,6 @@ type Props = {
   }[]; // 차트에 그려지는 Data
   xDataKey: string; // x축 기준 (가로일 경우 값)
   yDataKey: string; // y축 기준 (가로일 경우 항목)
-  width?: number; // 차트 너비
   height?: number; // 차트 높이
   className?: string; // CSS 클래스
 };
@@ -27,41 +26,40 @@ export default function BarGraph({
   data,
   xDataKey,
   yDataKey,
-  width = 500,
   height = 300,
-  className = 'p-5',
 }: Props) {
   return (
-    <ResponsiveContainer width='100%' height={height}>
-      <BarChart
-        layout='vertical'
-        width={width}
-        height={height}
-        data={data}
-        className={className}
-      >
-        <XAxis type='number' dataKey={xDataKey} hide />
-        <YAxis type='category' dataKey={yDataKey} />
-        <Tooltip />
-        <Bar
-          dataKey='predictedNps'
-          label={({ x, y, width, height, value }) => (
-            <text
-              x={x + width + 8}
-              y={y! + height / 2}
-              fill='#999'
-              fontSize={16}
-              dominantBaseline='middle'
-            >
-              월 {(value / 10000).toLocaleString()} 만원
-            </text>
-          )}
+    <div className='p-5'>
+      <ResponsiveContainer width='100%' height={height}>
+        <BarChart
+          layout='vertical'
+          height={height}
+          data={data}
+          margin={{ top: 20, right: 100, left: 0, bottom: 5 }}
         >
-          {data.map((entry) => (
-            <Cell key={entry.age} fill={entry.barColor} />
-          ))}
-        </Bar>
-      </BarChart>
-    </ResponsiveContainer>
+          <XAxis type='number' dataKey={xDataKey} hide />
+          <YAxis type='category' dataKey={yDataKey} width={50} />
+          <Tooltip />
+          <Bar
+            dataKey='predictedNps'
+            label={({ x, y, width, height, value }) => (
+              <text
+                x={x + width + 8}
+                y={y! + height / 2}
+                fill='#999'
+                fontSize={16}
+                dominantBaseline='middle'
+              >
+                월 {(value / 10000).toLocaleString()} 만원
+              </text>
+            )}
+          >
+            {data.map((entry) => (
+              <Cell key={entry.age} fill={entry.barColor} />
+            ))}
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
