41810903fc3c183e38dc150a3e86d49290102a3a
bokyeommmmm <bokyeommmmm@github.com>
2025-06-17 10:01:45 +0900

    Fix: SalaryBarGraph 기능 수정

--- hps/components/molcules/SalaryBarGraph.tsx
+++ hps/components/molcules/SalaryBarGraph.tsx
@@ -11,17 +11,20 @@ import {
 } from 'recharts';
 
 type Props = {
-  data: { moreSpent: number; leastSpent: number; name?: string }[];
+  //사용한 금액 , 남은 금액, x축 label
+  data: { used: number; remain: number; name?: string }[];
   width?: number;
   height?: number;
   className?: string;
-  colors: { moreSpent: string; leastSpent: string };
+  colors: { used: string; remain: string };
 };
 
 const CustomBar = (
+  //직접 SVG 경로를 그리는 커스텀 막대
   props: Partial<BarProps> & { radius: [number, number, number, number] }
 ) => {
   const {
+    //막대의 위치, 크기, 채울 색, 모서리 둥근 값.
     x = 0,
     y = 0,
     width = 0,
@@ -30,8 +33,11 @@ const CustomBar = (
     radius = [0, 0, 0, 0],
   } = props;
 
+  //top left, top right ...
   const [tl, tr, br, bl] = radius;
 
+  //SVG path 문자열로 둥근 사각형을 직접 그림
+  //각 꼭짓점 radius 있으면 둥글게 아님 직각.
   const path = `
     M${Number(x) + tl},${y}
     h${width - tl - tr}
@@ -49,6 +55,7 @@ const CustomBar = (
     <path
       d={path}
       fill={fill}
+      //테두리 색
       stroke={fill === '#FFFFFF' ? '#56B8AB' : fill}
       strokeWidth={1}
     />
@@ -57,9 +64,9 @@ const CustomBar = (
 
 export default function SalaryBarChart({
   data,
-  className = 'p-5',
-  width = 500,
-  height = 100,
+  className = '',
+  width = 600,
+  height = 40,
   colors,
 }: Props) {
   return (
@@ -68,22 +75,25 @@ export default function SalaryBarChart({
         layout='vertical'
         width={width}
         data={data}
+        margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
         className={className}
       >
         <XAxis type='number' hide />
         <YAxis type='category' dataKey='name' hide />
         <Tooltip formatter={(value: number) => value.toLocaleString() + '원'} />
         <Bar
-          dataKey='moreSpent'
+          //막대그래프 왼쪽 파트
+          dataKey='used'
           stackId='a'
           shape={<CustomBar radius={[10, 0, 0, 10]} />}
-          fill={colors.moreSpent}
+          fill={colors.used}
         />
         <Bar
-          dataKey='leastSpent'
+          //오른쪽 파트
+          dataKey='remain'
           stackId='a'
           shape={<CustomBar radius={[0, 10, 10, 0]} />}
-          fill={colors.leastSpent}
+          fill={colors.remain}
         />
       </BarChart>
     </ResponsiveContainer>
