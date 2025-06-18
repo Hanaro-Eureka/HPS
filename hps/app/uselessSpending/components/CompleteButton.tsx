a561d3e8b993e19c3cd682a8ff60053d7f482743
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-06-18 10:27:17 +0900

    Feat: 의미없는 소비 고르고 DB 저장 기능 구현

--- hps/app/uselessSpending/components/CompleteButton.tsx
+++ hps/app/uselessSpending/components/CompleteButton.tsx
@@ -1,20 +1,20 @@
-'use client';
-
 import Button from '@/components/atoms/Button';
-import { useRouter } from 'next/navigation';
+import { saveUselessSpending } from '../utils/saveUselessSpending';
 
 type Props = {
   selectedIds: (string | number)[];
 };
 
 export default function CompleteButton({ selectedIds }: Props) {
-  const router = useRouter();
+  const handleClick = async () => {
+    await saveUselessSpending(selectedIds);
+  };
 
   return (
     <Button
       bgColor='bg-button-lightgray'
       className='w-35 py-3 text-black-font rounded-3xl font-[500] text-base hover:bg-button-gray'
-      onClick={() => router.push('/hanaChallenge')}
+      onClick={handleClick}
     >
       완료
     </Button>
