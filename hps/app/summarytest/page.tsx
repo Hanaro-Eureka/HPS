6d302559b9e2e5a0e1ff0e7b86ab47b2f98f066b
kimkiyeonn <paynjoy2@naver.com>
2025-06-21 23:48:34 +0900

    Feat: 금액 별 원 구현 및 충돌 해결

--- hps/app/summarytest/page.tsx
+++ hps/app/summarytest/page.tsx
@@ -1,10 +1,35 @@
+// import SalaryCircle from '../components/SalaryCircle';
+// export default async function SummaryTestPage() {
+//   return (
+//     <div className='p-6'>
+//       <SalaryCircle></SalaryCircle>
+//     </div>
+//   );
+// }
+// import { getIncomeSourcesByUserId } from '@/lib/actions/salary-summary';
+// export default async function SummaryTestPage() {
+//   const now = new Date();
+//   const start = new Date(now.getFullYear(), now.getMonth(), 1);
+//   const data = await getIncomeSourcesByUserId(1, start);
+//   return (
+//     <div className='p-6'>
+//       <h1 className='text-xl font-bold mb-4'>이번 달 수입 구조</h1>
+//       {/* <SalaryBubbleChart data={data} /> */}
+//     </div>
+//   );
+// }
 import { getIncomeSourcesByUserId } from '@/lib/actions/salary-summary';
 import SalaryCircle from '../components/SalaryCircle';
 
 export default async function SummaryTestPage() {
+  const now = new Date();
+  const start = new Date(now.getFullYear(), now.getMonth(), 1);
+  const data = await getIncomeSourcesByUserId(1, start);
+
   return (
     <div className='p-6'>
-      <SalaryCircle></SalaryCircle>
+      <h1 className='text-xl font-bold mb-4'>이번 달 수입 구조</h1>
+      <SalaryCircle data={data} />
     </div>
   );
 }
