74b3cfe1421a689ad4ce2c0ae4ae784ab7e8687d
kimkiyeonn <paynjoy2@naver.com>
2025-06-23 15:36:48 +0900

    Feat: Input을 props로 받을 수 있게 수정

--- hps/components/molcules/StarChat.tsx
+++ hps/components/molcules/StarChat.tsx
@@ -15,12 +15,18 @@ export default function StarChat() {
     //     </div>
     //   </div>
     // </div>
-    <div className='flex'>
-      <Image src='/svgs/ic_secretary.svg' alt='별비서' width={33} height={33} />
-      <div className='bg-hana-ai p-4 rounded-t-3xl rounded-br-3xl w-48'>
-        <h3 className='text-sm font-[500] text-black'>
-          Hello,i’m SLML 입니다.
-        </h3>
+    // <div className='flex  p-4'>
+    <div className='flex gap-4 items-end'>
+      <div className='flex items-center'>
+        <Image
+          src='/svgs/ic_secretary.svg'
+          alt='별비서'
+          width={33}
+          height={33}
+        />
+      </div>
+      <div className='bg-hana-ai p-4 rounded-t-3xl rounded-br-3xl w-58'>
+        <p className='text-sm font-[500] text-black'>Hello,i’m SLML 입니다.</p>
       </div>
     </div>
   );
