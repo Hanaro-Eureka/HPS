abc249769b07fbcdbc6590c51285bfa4bf404a82
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-06-23 16:20:02 +0900

    Chore: 메인페이지 수정

--- hps/app/page.tsx
+++ hps/app/page.tsx
@@ -6,7 +6,7 @@ import SalaryCircle from './components/SalaryCircle';
 export default function Home() {
   return (
     <>
-      <div className='min-h-screen flex flex-col overflow-hidden bg-gradient-salary '>
+      <div className='min-h-screen flex flex-col'>
         <MainBar />
         <div className='flex items-center justify-center'>
           <SalaryBox />
