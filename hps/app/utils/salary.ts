da44989e0473c9859cb2e170796ca8205b060217
dlwlals1289 <dlwlals1289@gmail.com>
2025-06-23 16:33:16 +0900

    Feat: MainPage - 카드든 하나몬스터 지난달 수입 대비 지출 반영해서 카드색 변경

--- hps/app/utils/salary.ts
+++ hps/app/utils/salary.ts
@@ -1,4 +1,8 @@
-import { getSalariesWithUserId } from '@/lib/actions/salary-actions';
+import {
+  getMonthlySalary,
+  getSalariesWithUserId,
+} from '@/lib/actions/salary-actions';
+import { getStartAndEndOfMonth } from '../spendList/utils/spending';
 
 export const getFirstDayOfThisMonth = () => {
   const now = new Date();
@@ -24,4 +28,25 @@ export const getSumOfThisMonthSalaries = async (userId: number) => {
   );
 
   return sumOfSalaries;
+};
+
+export const getLastIncome = async (userId: number) => {
+  const now = new Date();
+
+  // 이번 달
+  const thisMonth = getStartAndEndOfMonth(now);
+
+  // 지난 달
+  const lastMonthDate = new Date(now.getFullYear(), now.getMonth() - 1, 1);
+  const lastMonth = getStartAndEndOfMonth(lastMonthDate);
+
+  // 두 달치 급여 가져오기
+  const [thisMonthSalaryList, lastMonthSalaryList] = await Promise.all([
+    getMonthlySalary(userId, thisMonth.start, thisMonth.end),
+    getMonthlySalary(userId, lastMonth.start, lastMonth.end),
+  ]);
+
+  const allSalaryList = [...thisMonthSalaryList, ...lastMonthSalaryList];
+
+  return allSalaryList;
 };
