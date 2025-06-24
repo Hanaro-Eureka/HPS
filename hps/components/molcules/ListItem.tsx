b55cd47da4b6c4b849bc6b72a84cac5f808e7628
dlwlals1289 <dlwlals1289@gmail.com>
2025-06-24 09:42:38 +0900

    Design: 수입 내역 리스트 - 수정 시 가격 텍스트 밀리는 현상 수정

--- hps/components/molcules/ListItem.tsx
+++ hps/components/molcules/ListItem.tsx
@@ -52,70 +52,70 @@ export default function ListItem({
         className
       )}
     >
-      <div className='flex items-center gap-3'>
+      <div className='flex items-center gap-3 w-full'>
         <div className='w-9 h-9 rounded-full flex items-center justify-center'>
           {icon}
         </div>
-        <div className='flex flex-col gap-1'>
-          {pathName === '/incomeList' ? (
-            <div>
-              {isEdited ? (
-                <form action={handleSubmit}>
-                  <input type='hidden' name='id' value={id} />
-                  <Input
-                    placeholder={newValue}
-                    className='border-1 w-36'
-                    name='value'
-                  />
-                </form>
-              ) : (
-                <div className='flex'>
-                  <Text
-                    tag='strong'
-                    className='text-black-font text-base font-[500]'
-                  >
-                    {newValue}
-                  </Text>
-                  <Button bgColor='white' onClick={() => setEdit(!isEdited)}>
-                    <Image
-                      src={'/svgs/ic_pencil.svg'}
-                      alt='수입 내역 수정하기'
-                      width={16}
-                      height={16}
+        <section className='flex flex-col w-full'>
+          <div className='flex justify-between items-center'>
+            {pathName === '/incomeList' ? (
+              <div className='flex-1'>
+                {isEdited ? (
+                  <form action={handleSubmit}>
+                    <input type='hidden' name='id' value={id} />
+                    <Input
+                      placeholder={newValue}
+                      className='border-1 w-full'
+                      name='value'
                     />
-                  </Button>
-                </div>
-              )}
-            </div>
-          ) : (
-            <div className='flex'>
-              <Text
-                tag='strong'
-                className='text-black-font text-base font-[500]'
-              >
-                {label}
+                  </form>
+                ) : (
+                  <div className='flex w-full flex-1'>
+                    <Text
+                      tag='strong'
+                      className='text-black-font text-base font-[500]'
+                    >
+                      {newValue}
+                    </Text>
+                    <Button bgColor='white' onClick={() => setEdit(!isEdited)}>
+                      <Image
+                        src={'/svgs/ic_pencil.svg'}
+                        alt='수입 내역 수정하기'
+                        width={16}
+                        height={16}
+                      />
+                    </Button>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <div className='flex-1'>
+                <Text
+                  tag='strong'
+                  className='text-black-font text-base font-[500] w-full'
+                >
+                  {label}
+                </Text>
+              </div>
+            )}
+            <Text
+              tag='span'
+              className='text-right text-base text-hana-green font-[500] whitespace-nowrap'
+            >
+              {amount.toLocaleString()}원
+            </Text>
+          </div>
+          <div className='flex justify-between'>
+            <Text tag='span' className='text-xs text-gray-time font-[500] '>
+              {time}
+            </Text>
+            {description && (
+              <Text tag='span' className='text-xs text-gray-time font-[400]'>
+                지난 달보다 {description}원
               </Text>
-            </div>
-          )}
-
-          <Text tag='span' className='text-xs text-gray-time font-[500]'>
-            {time}
-          </Text>
-        </div>
-      </div>
-
-      <div className='flex flex-col'>
-        <Text
-          tag='span'
-          className='text-right text-base text-hana-green font-[500]'
-        >
-          {amount.toLocaleString()}원
-        </Text>
-        {description && (
-          <Text tag='span' className='text-xs text-gray-time font-[400]'>
-            지난 달보다 {description}원
-          </Text>
-        )}
+            )}
+          </div>
+        </section>
       </div>
     </div>
   );
