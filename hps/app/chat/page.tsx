bac648d0de7be84db4b738cd8112450fc00bb5b6
doormoonn <robort123@gachon.ac.kr>
2025-06-24 13:57:51 +0900

    feat: 챗봇 완료

--- hps/app/chat/page.tsx
+++ hps/app/chat/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import StarChat from '@/app/chat/componets/StarChat';
-import UserChat from '@/app/chat/componets/UserChat';
+import StarChat from '@/app/chat/components/StarChat';
+import UserChat from '@/app/chat/components/UserChat';
 import Text from '@/components/atoms/Text';
 import { useChat } from '@ai-sdk/react';
 import { useState } from 'react';
