69fafc76f24b6a7d01864e920334df4c8a45e76c
kimkiyeonn <paynjoy2@naver.com>
2025-06-25 16:27:45 +0900

    Fix: 스크롤 제거

--- hps/app/page.tsx
+++ hps/app/page.tsx
@@ -6,16 +6,12 @@ import SalaryCircle from './components/SalaryCircle';
 export default function Home() {
   return (
     <>
-      <div className='h-screen p-7 flex flex-col'>
+      <div className='relative min-h-screen p-7 flex flex-col'>
         <MainBar />
-        <div className='flex items-center justify-center'>
+        <div className='flex items-center justify-center '>
           <SalaryBox />
         </div>
-        <div className='flex pt-3 pb-24'>
-          <div className='flex h-[calc(100vh-400px)]'>
-            <SalaryCircle />
-          </div>
-        </div>
+        <SalaryCircle />
       </div>
       <BottomTabBar />
     </>
