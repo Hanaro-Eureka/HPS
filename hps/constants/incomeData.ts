7bd179639e3fb74589321f8de0127de072ebc9f4
kimkiyeonn <paynjoy2@naver.com>
2025-06-26 15:18:10 +0900

    Chore: user2,3 데이터 추가

--- hps/constants/incomeData.ts
+++ hps/constants/incomeData.ts
@@ -58,7 +58,7 @@ export const incomeData = [
   {
     trans_dtime: '20250603170530',
     trans_type: '03',
-    trans_amt: 2880000,
+    trans_amt: 1000000,
     branch_name: '하나은행 본점',
     trans_memo: '개미인력',
     id: 8,
@@ -138,7 +138,7 @@ export const incomeData = [
   {
     trans_dtime: '20250603050944',
     trans_type: '03',
-    trans_amt: 1240000,
+    trans_amt: 500000,
     branch_name: '하나은행 본사',
     trans_memo: '유튜브',
     id: 18,
@@ -202,9 +202,9 @@ export const incomeData = [
   {
     trans_dtime: '20250625130237',
     trans_type: '03',
-    trans_amt: 1880000,
+    trans_amt: 700000,
     branch_name: '하나은행 본점',
-    trans_memo: '수고하였어요',
+    trans_memo: '수고하셨어요',
     id: 26,
   },
   {
@@ -250,9 +250,41 @@ export const incomeData = [
   {
     trans_dtime: '20250611150944',
     trans_type: '03',
-    trans_amt: 1700000,
+    trans_amt: 1400000,
     branch_name: '하나은행 본사',
     trans_memo: '인스타그램',
     id: 32,
+  },
+  {
+    trans_dtime: '20250601140944',
+    trans_type: '03',
+    trans_amt: 1423900,
+    branch_name: '하나은행 본사',
+    trans_memo: '외주 프로젝트',
+    id: 33,
+  },
+  {
+    trans_dtime: '20250602190944',
+    trans_type: '03',
+    trans_amt: 584930,
+    branch_name: '하나은행 마곡나루점',
+    trans_memo: '틱톡',
+    id: 34,
+  },
+  {
+    trans_dtime: '20250602190944',
+    trans_type: '03',
+    trans_amt: 888800,
+    branch_name: '하나은행 마포점',
+    trans_memo: '(주)디자인펀치',
+    id: 35,
+  },
+  {
+    trans_dtime: '20250615190944',
+    trans_type: '03',
+    trans_amt: 1120800,
+    branch_name: '하나은행 신촌점',
+    trans_memo: '(주)블루웨일',
+    id: 36,
   },
 ];
