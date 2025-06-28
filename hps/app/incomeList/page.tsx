280be64ac4978ed8ca6c4089d42a3b1c63e7739b
doormoonn <robort123@gachon.ac.kr>
2025-06-28 17:06:13 +0900

    refactor: 수입내역scroll추가

--- hps/app/incomeList/page.tsx
+++ hps/app/incomeList/page.tsx
@@ -42,9 +42,11 @@ export default async function incomePage({ searchParams }: Props) {
             }}
           />
         </div>
-        <IncomeList month={selectedMonth} />
+        <div className='overflow-y-auto w-full h-80'>
+          <IncomeList month={selectedMonth} />
+        </div>
       </div>
-      <div className='flex justify-center mt-5 mb-10'>
+      <div className='flex justify-center mt-8 mb-10'>
         <GoToIncomeButton />
       </div>
     </HeaderLayout>
